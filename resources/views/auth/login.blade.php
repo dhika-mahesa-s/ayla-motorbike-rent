69eabf18204ed0190056733d7f00b714fa4dc27e
Dhika Mahesa S <dhikamahesa45@gmail.com>
2025-11-08 09:30:40 +0700

    ubah konfigurasi tailwindcs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy"
+    content="upgrade-insecure-requests">
     <title>Login - Ayla Motorbike Rent</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
         .gradient-bg {
             background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
